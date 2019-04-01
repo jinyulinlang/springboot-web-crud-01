846871260800675305e53abcfa0b6d798511f2f0
gaopan <gaopan125@aliyun.com>
2019-04-01 22:46:35 +0800

    gaopan

--- src/main/java/com/gao/controller/LoginController.java
+++ src/main/java/com/gao/controller/LoginController.java
@@ -1,14 +1,11 @@
 package com.gao.controller;
 
-import com.gao.form.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -19,7 +16,7 @@ public class LoginController {
         if (!StringUtils.isEmpty (name) && "123456".equals (password)) {
             return "redirect:/main.html";
         } else {
-            session.setAttribute ("msg", "用户名或者密码错误!");
+            session.setAttribute ("msg", "用户名错误！");
             return "redirect:/";
         }
     }
